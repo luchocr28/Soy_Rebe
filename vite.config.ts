c5f1c8d7bdd86b52f2193daf5630ef6228a0a66b
luchocr28 <luchocr28@gmail.com>
2026-07-10 12:51:22 -0600

    vite config updated base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Soy_Rebe',
+  base: '/Soy_Rebe-main/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
